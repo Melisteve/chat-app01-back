db69e5c24c9330e7036e7d00393df727d07656d8
Melisteve <melisteve6@gmail.com>
2023-01-03 19:18:52 +0100

    backend whit socket.io

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const socket = require("socket.io");
 const userRoutes = require("./routes/userRoutes");
+const messageRoute = require("./routes/messagesRoutes");
 
 const app = express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -25,4 +30,26 @@ mongoose
 
 const server = app.listen(process.env.PORT, () => {
   console.log(` Server Started on Port ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+  });
 });
